ef52e7acaf45f25c2097228dddb43f4a1bb208e2
Ian Holladay <holladayian@gmail.com>
2021-02-08 16:54:16 -0700

    Add basic About component

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.scss';
+
+export const About = () => {
+    return (
+      <section class="about">
+        <h1>About</h1>
+      </section>
+    )
+}
