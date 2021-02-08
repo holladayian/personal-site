df45de58eb74898ea41f957e3ddcb820bad5233c
Ian Holladay <holladayian@gmail.com>
2021-02-08 16:56:30 -0700

    implement link to route to different pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Header.scss';
 
 export const Header = () => {
     return (
       <section class="header">
-        This is the header
+        <h1>Ian Holladay</h1>
+        <h2>Web Developer</h2>
+            <Link to= '/projects' >
+            <button className="projects-button">Projects</button>
+            </Link>
+            <Link to= '/about' >
+            <button className="about-button">About</button>
+            </Link>
       </section>
     )
 }
