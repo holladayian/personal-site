a1f987f8a852a109a67e3a44e47da1a8ff2e1263
Ian Holladay <holladayian@gmail.com>
2021-02-09 16:57:17 -0700

    Design basic layout of Project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Projects.scss';
 
 export const Projects = ({projects}) => {
   // return (
@@ -8,7 +9,27 @@ export const Projects = ({projects}) => {
   // )
   return projects.map(project => {
     return (
-      <section>{project.name}</section>
+      <section class="project-card">
+        <h1 class="name">
+          {project.name}
+        </h1>
+        <section class="info">
+          <section class="tech">
+          <h2>Tech</h2>
+          {project.tech.map(t => <p>{t}</p>)}
+          
+          </section>
+          <section class="description">
+          <h2>Description</h2>
+          {project.description}
+
+          </section>
+          <section class="images">
+          <h2>Images</h2>
+
+          </section>
+        </section>
+      </section>
     )
   })
 }
