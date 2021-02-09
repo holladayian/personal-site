d17b335a3e7299e570aa62984cc474238791f9df
Ian Holladay <holladayian@gmail.com>
2021-02-09 14:54:39 -0700

    Add classes to sections

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,18 @@ import '../styles/Header.scss';
 export const Header = () => {
     return (
       <section class="header">
-        <h1>Ian Holladay</h1>
-        <h2>Web Developer</h2>
-            <Link to= '/projects' >
+        <section class="intros">
+          <h1 class="ian">Ian Holladay</h1>
+          <h2>Web Developer</h2>
+        </section>
+        <section class="button-section">
+          <Link to= '/projects' >
             <button className="projects-button">Projects</button>
-            </Link>
-            <Link to= '/about' >
+          </Link>
+          <Link to= '/about' >
             <button className="about-button">About</button>
-            </Link>
+          </Link>
+        </section>
       </section>
     )
 }
