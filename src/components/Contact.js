52afd4cfdc6029d22428f6e3a29117eadd86631b
Ian Holladay <holladayian@gmail.com>
2021-02-09 14:59:37 -0700

    Create Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import '../styles/Header.scss';
+
+export const Contact = () => {
+    return (
+      <section class="contact">
+        <h1>Contact</h1>
+      </section>
+    )
+}
