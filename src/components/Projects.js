69ac27bfccc658c2a728e014c7b0bbfc88976b43
Ian Holladay <holladayian@gmail.com>
2021-02-09 16:31:40 -0700

    map incomming projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export const Projects = (projects) => {
-  return (
-    <section class="projects">
-      <h1>Projects</h1>
-    </section>
-  )
+export const Projects = ({projects}) => {
   // return (
-  //   projects.map(project => {
-  //   return ``
-  //   })
+  //   <section class="projects">
+  //     <h1>Projects</h1>
+  //   </section>
   // )
+  return projects.map(project => {
+    return (
+      <section>{project.name}</section>
+    )
+  })
 }
