889632e70fcf2ed33dd9adb0c176e690b4996fb0
Ian Holladay <holladayian@gmail.com>
2021-02-09 15:00:55 -0700

    Add route for Contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Header } from './components/Header.js';
 import { Projects } from './components/Projects.js';
 import { About } from './components/About.js';
+import { Contact } from './components/Contact.js';
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -26,14 +27,18 @@ export class App extends Component{
         <div className="App">
           <Header />
           <Switch>
-          <Route
-            path='/projects'
-            render={() => <Projects projects={this.state.projects}/>}
-          />
-          <Route
-            path='/about'
-            render={() => <About />}
-          />
+            <Route
+              path='/projects'
+              render={() => <Projects projects={this.state.projects}/>}
+            />
+            <Route
+              path='/about'
+              render={() => <About />}
+            />
+            <Route
+              path='/contact'
+              render={() => <Contact />}
+            />
           </Switch>
         </div>
       );
