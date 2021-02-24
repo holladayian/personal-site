470f18823ad00720683029c28b238ff5be4176b5
Ian Holladay <holladayian@gmail.com>
2021-02-24 13:20:46 -0700

    Add Intention Timer

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export class App extends Component{
         {
           name: 'Rancid Tomatillos',
           description: 'Rotten tomatoes knock off. Blah blah blah, blah blah. Blah, blah blah blah, blah, blah blah blah...',
-          tech: ['JavaScript', 'React', 'CSS'],
+          tech: ['JavaScript', 'React', 'CSS', 'JSX'],
           deployed: 'https://rancid-t-omatillos.herokuapp.com/',
           repository: 'https://github.com/holladayian/ih-rancidtomatillos',
           images: [
@@ -26,6 +26,47 @@ export class App extends Component{
             source: 'https://media.giphy.com/media/KhQN8qc4TjfDwykk6P/giphy.gif',
             description: "When the user inputs the following crendntial, `name` 'diana', `email`'diana@turing@io', and `password` '111111'. The user is greeted with a welcomin message that shows the user that has logged in succesfully."
             }
+          ]
+        },
+        {
+          name: 'Intention Timer',
+          description: 'The goal of this project was to build an application that allows a user to set goals for their health and productivity, tied to an amount of time. Users will select an activity category, set the amount of time they want to spend on that activity, and start the timer. The app will log that activity to keep track of how the user has been spending their time.',
+          tech: ['JavaScript', 'React', 'CSS'],
+          deployed: '',
+          repository: 'https://github.com/holladayian/intention-timer',
+          images: [
+            {
+            source: '',
+            description: 'Our first task was to build the layout of our Intention Timer application using HTML and CSS, without building in any JavaScript functionality. We used semantic HTML elements throughout where appropriate to ensure we make the application accessible, and tried to use as simple yet effective CSS as possible.'
+            },
+            {
+            source: '',
+            description: "This portion of the project involved several steps. First, we added functionality to the form by making each category button change border, text, and icon color when clicked, to the colors specified in our project brief."
+            },
+            {
+            source: '',
+            description: 'Next, we added several form validation functions. The first disables any character that is not a number from being entered in the minutes and seconds form fields. The second checks to see if all of the form fields have an input, and if not, checks each individually and inserts error messages for those missing a value.'
+            },
+            {
+            source: '',
+            description: 'When a user has filled out all fields and clicks the "Start Activity" button, their information is stored as an instance of our Activity class. Additionally, that button click triggers a timer clock to display in place of the form. The timer clock has a circle outline that matches the color of the selected category, and displays the user-provided description and time.'
+            },
+            {
+            source: '',
+            description: 'Next, we created a function to make our timer functional, using the setInterval method. We were able to achieve this so when a user clicks "Start", the timer begins counting down and continues down to 0 seconds. At this stage of the project, we added a temporary alert that pops up when the timer completes.'
+            },
+            {
+            source: '',
+            description: 'Then, we removed the temporary alert and instead added functionality to display a congratulatory message when the timer reaches 0, along with a new "Log Activity" button.'
+            },
+            {
+            source: '',
+            description: 'When the user clicks this "Log Activity" button, a past Activity card appears on the right-hand side of the application, which displays the activity description, time, and a color-coded line matching the category type of that activity. A new button to Create a New Activity will also appear in place of the timer.'
+            },
+            {
+            source: '',
+            description: 'We also made our application responsive, so it displays correctly on mobile as well.'
+            },
           ]
         },
         {
